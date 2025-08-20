a4067ee9aff13a1a5621d9a9e6c86fcd002314ef
이혜정 <lhj1227lhj@naver.com>
2025-08-20 16:15:54 +0900

    init

---
+++ src/main/java/com/ssu10/personal_career/PersonalCareerApplication.java
@@ -0,0 +1,13 @@
+package com.ssu10.personal_career;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCareerApplication.class, args);
+	}
+
+}
